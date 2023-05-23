cde3fbb2283036fca84a9f48322b31a0c58ca606
memmedkerimtagiyev <7M16ZU6@code.edu.az>
2023-05-23 10:15:34 +0400

    tasklar basa bela

--- ImtahanSinaq1/assets/javascript/index.js
+++ ImtahanSinaq1/assets/javascript/index.js
@@ -9,38 +9,40 @@ barIcon.addEventListener("click", () => {
 
 openIcon.forEach((btn) => {
   btn.addEventListener("click", (e) => {
-    openIcon.forEach((btn) => {
-      console.log("salama", btn.parentElement.parentElement);
-      btn.parentElement.parentElement.style.height = "45px";
-    });
-
+    // openIcon.forEach((btn) => {
+    //   console.log("salama", btn.parentElement.parentElement);
+    //   btn.parentElement.parentElement.style.height = "45px";
+    // });
+    console.log(btn);
+    e.target.classList.toggle("trans");
     e.target.parentElement.parentElement.classList.toggle("openjsSteps");
   });
 });
 
 const childrenBox = document.querySelector(".childrenBox");
 const childBox = document.createElement("div");
-childBox.className="childBox"
+childBox.className = "childBox";
 const photo = document.createElement("div");
-photo.className="photo"
+photo.className = "photo";
 const img = document.createElement("img");
-img.tagName="img"
+img.tagName = "img";
 const nameyear = document.createElement("a");
-nameyear.tagName="a"
-const year=document.createElement("span")
-year.tagName="span"
+nameyear.tagName = "a";
+const year = document.createElement("span");
+year.tagName = "span";
 
- nameyear.innerText="mursel,"
- year.innerText="15 year old"
- img.setAttribute("src", "https://preview.colorlib.com/theme/adopted/img/children_4.jpg")
+nameyear.innerText = "mursel,";
+year.innerText = "15 year old";
+img.setAttribute(
+  "src",
+  "https://preview.colorlib.com/theme/adopted/img/children_4.jpg"
+);
 
-childBox.appendChild(photo)
-photo.appendChild(img)
+childBox.appendChild(photo);
+photo.appendChild(img);
 childBox.appendChild(nameyear);
-nameyear.appendChild(year)
-childrenBox.appendChild(childBox)
-
-
+nameyear.appendChild(year);
+childrenBox.appendChild(childBox);
 
 // fetch("https://northwind.vercel.app/api/products")
 //   .then((res) => res.json())
